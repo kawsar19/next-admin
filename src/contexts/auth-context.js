19c0041fee538b1028928e5180bb209d46c1970b
kawsar ahmed <kawsar.qtec@gmail.com>
2023-05-26 18:24:53 +0600

    added home page

--- src/contexts/auth-context.js
+++ src/contexts/auth-context.js
@@ -1,61 +1,18 @@
 import { createContext, useContext, useEffect, useReducer, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-const HANDLERS = {
-  INITIALIZE: 'INITIALIZE',
-  SIGN_IN: 'SIGN_IN',
-  SIGN_OUT: 'SIGN_OUT'
-};
-
 const initialState = {
-  isAuthenticated: false,
-  isLoading: true,
-  user: null
-};
-
-const handlers = {
-  [HANDLERS.INITIALIZE]: (state, action) => {
-    const user = action.payload;
-
-    return {
-      ...state,
-      ...(
-        // if payload (user) is provided, then is authenticated
-        user
-          ? ({
-            isAuthenticated: true,
-            isLoading: false,
-            user
-          })
-          : ({
-            isLoading: false
-          })
-      )
-    };
-  },
-  [HANDLERS.SIGN_IN]: (state, action) => {
-    const user = action.payload;
-
-    return {
-      ...state,
-      isAuthenticated: true,
-      user
-    };
-  },
-  [HANDLERS.SIGN_OUT]: (state) => {
-    return {
-      ...state,
-      isAuthenticated: false,
-      user: null
-    };
+  isAuthenticated: true,
+  isLoading: false,
+  user: {
+    id: '5e86809283e28b96d2d38537',
+    avatar: '/assets/avatars/avatar-anika-visser.png',
+    name: 'Anika Visser',
+    email: 'anika.visser@devias.io'
   }
 };
 
-const reducer = (state, action) => (
-  handlers[action.type] ? handlers[action.type](state, action) : state
-);
-
-// The role of this context is to propagate authentication state through the App tree.
+const reducer = (state, action) => state;
 
 export const AuthContext = createContext({ undefined });
 
@@ -72,103 +29,19 @@ export const AuthProvider = (props) => {
 
     initialized.current = true;
 
-    let isAuthenticated = false;
-
-    try {
-      isAuthenticated = window.sessionStorage.getItem('authenticated') === 'true';
-    } catch (err) {
-      console.error(err);
-    }
-
-    if (isAuthenticated) {
-      const user = {
-        id: '5e86809283e28b96d2d38537',
-        avatar: '/assets/avatars/avatar-anika-visser.png',
-        name: 'Anika Visser',
-        email: 'anika.visser@devias.io'
-      };
-
-      dispatch({
-        type: HANDLERS.INITIALIZE,
-        payload: user
-      });
-    } else {
-      dispatch({
-        type: HANDLERS.INITIALIZE
-      });
-    }
-  };
-
-  useEffect(
-    () => {
-      initialize();
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
-
-  const skip = () => {
-    try {
-      window.sessionStorage.setItem('authenticated', 'true');
-    } catch (err) {
-      console.error(err);
-    }
-
-    const user = {
-      id: '5e86809283e28b96d2d38537',
-      avatar: '/assets/avatars/avatar-anika-visser.png',
-      name: 'Anika Visser',
-      email: 'anika.visser@devias.io'
-    };
-
     dispatch({
-      type: HANDLERS.SIGN_IN,
-      payload: user
+      type: 'INITIALIZE'
     });
   };
 
-  const signIn = async (email, password) => {
-    if (email !== 'demo@devias.io' || password !== 'Password123!') {
-      throw new Error('Please check your email and password');
-    }
-
-    try {
-      window.sessionStorage.setItem('authenticated', 'true');
-    } catch (err) {
-      console.error(err);
-    }
-
-    const user = {
-      id: '5e86809283e28b96d2d38537',
-      avatar: '/assets/avatars/avatar-anika-visser.png',
-      name: 'Anika Visser',
-      email: 'anika.visser@devias.io'
-    };
-
-    dispatch({
-      type: HANDLERS.SIGN_IN,
-      payload: user
-    });
-  };
-
-  const signUp = async (email, name, password) => {
-    throw new Error('Sign up is not implemented');
-  };
-
-  const signOut = () => {
-    dispatch({
-      type: HANDLERS.SIGN_OUT
-    });
-  };
+  useEffect(() => {
+    initialize();
+  }, []);
 
   return (
     <AuthContext.Provider
       value={{
-        ...state,
-        skip,
-        signIn,
-        signUp,
-        signOut
+        ...state
       }}
     >
       {children}
--- src/pages/admin/login.js
+++ src/pages/admin/login.js
@@ -1,45 +1,129 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Container, CircularProgress, TextField, Typography } from '@mui/material';
+import axios from 'axios';
+import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { toast } from 'react-toastify';
+// import { useDispatch } from 'react-redux';
 
-export default function Login() {
-  const [username, setUsername] = useState('');
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
-
-  const handleSubmit = (e) => {
+  // const dispatch = useDispatch();
+  
+  const handleLogin = async (e) => {
     e.preventDefault();
-    // Here you can add your login logic
-    // For example, you can validate the username and password
-    // against a database or an API endpoint
+    
+    try {
+      if (password.length < 6) {
+        toast.error("Please Provide a Valid Password!");
+        return false;
+      }
+      
+      setLoading(true); // Start the loader
+      
+      const data = {
+        email,
+        password
+      };
 
-    // Assuming the login is successful, redirect to a protected page
-    router.push('/dashboard');
+      const response = await axios.post("http://localhost:5000/api/auth/login", data);
+      if (response.data?.login) {
+        toast.success("Logged In Successfully!");
+        localStorage.setItem("accessToken", response.data.token);
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        localStorage.setItem("isLoggedIn", true);
+        // dispatch(authActions.setAccessToken(response.data.token));
+        // dispatch(authActions.setUser(response.data.user));
+        // dispatch(authActions.setIsLoggedIn(true));
+        router.push("/admin");
+      }
+    } catch (error) {
+      console.log(error);
+      
+      if (error.response?.data) {
+        toast.error(error.response.data.message);
+      } else { 
+        toast.error("Server Error!");
+      }
+    } finally {
+      setLoading(false); // Finish the loader
+    }
   };
 
   return (
-    <div>
-      <h1> Admin Login Page</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Container 
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          flexDirection:'column'
+        }}
+      >
+        <Box
+          sx={{
+            width: 400,
+            p: 4,
+            boxShadow: 1,
+            bgcolor: 'background.paper',
+          }}
+        >
+          <Typography variant="h5" component="h1" gutterBottom>
+            Login
+          </Typography>
+          <form onSubmit={handleLogin}>
+            <TextField
+              required
+              label="Email"
+              type="email"
+              fullWidth
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              required
+              label="Password"
+              type="password"
+              fullWidth
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button type="submit" variant="contained" fullWidth disabled={loading}>
+              {loading ? (
+                <>
+                  <CircularProgress size={24} color="inherit" />
+                  <span>Loading...</span>
+                </>
+              ) : (
+                'Login'
+              )}
+            </Button>
+          </form>
+        </Box> 
+        <Box mt={2}>
+          <Typography variant="body2">
+            Demo User: admin@gmail.com
+            <br />
+            Password: 111111
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
   );
-}
+};
+
+export default LoginPage;
